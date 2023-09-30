4ea7a940ba1c5f72aa3106f5b92b04adeca69b29
Vaibhav Choudhary <vaibhavchoudhary@Vaibhavs-MacBook-Pro.local>
2023-09-29 20:03:14 -0400

    copying over from other repo

---
+++ test.js
@@ -0,0 +1,48 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must enter something!!");
+
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveInput();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveInput();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveInput();
+    }
+}, false);
+
+function saveInput(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function displayList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+displayList();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dateElement = document.getElementById("date");
+    const estOptions = { timeZone: "America/New_York" }; 
+    const estDate = new Date().toLocaleString("en-US", estOptions);
+    const formattedDate = "Today's date and time is: " + estDate.replace(/[^0-9]/g, '');
+    dateElement.textContent = formattedDate;
+});
+
+
